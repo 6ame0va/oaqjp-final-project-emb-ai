3069f406d3bb0def81475529f0b351ea182e6140
Ugochukwu Eke <ejahswill1@gmail.com>
2026-04-14 17:53:50 +0100

    Implement emotion detection function
    Added a function to detect emotions from text using an external API.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,25 @@
+import requests
+import json
+"""
+Function to run emotion detection using the appropriate Emotion Detection function.
+"""
 
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    someObj = {"raw_document": { "text": text_to_analyze }}
+    response = requests.post(url, json=someObj, headers=header)
+    # Parse response as json
+    parsed_response = json.loads(response.text)
+    # Collect specific traits to return
+    anger_score = parsed_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = parsed_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = parsed_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = parsed_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = parsed_response['emotionPredictions'][0]['emotion']['sadness']
+    # Collect all in a list for futher parsing
+    dominant = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+    # Collect the larget in the lisrt
+    dominant_score = max(dominant)
+
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': dominant_score}
